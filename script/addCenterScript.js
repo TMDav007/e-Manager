8f1d0cf1ee0b1c6a4d89325fd59d7ab72dba460d
Afolabi Toluwalase <opson_ryan@yahoo.com>
2018-01-29 11:37:52 -1200

    add click function,modal function to the js file

--- script/addCenterScript.js
+++ script/addCenterScript.js
@@ -3,4 +3,18 @@ $(window).load(function(){
         document.getElementById("addCenter").style.display= "block";
     });
 
+    $(".modal_editCenter").on("click", function(){
+        document.getElementById("editCenter").style.display= "block";
+    });
+
+    $(".modal_delCenter").on("click", function(){
+        document.getElementById("delCenter").style.display= "block";
+    });
+
+    $(".close").on("click", function(){
+        document.getElementById("addCenter").style.display = "none";
+        document.getElementById("editCenter").style.display = "none";
+        document.getElementById("delCenter").style.display= "none";
+    });
+
 });
