d5623c016cca85224815e11285cef5f30ea6dc2e
Afolabi Toluwalase <opson_ryan@yahoo.com>
2018-02-12 09:55:05 -1200

    added the script for readmore, navbar scroll effect

--- script/indexScript.js
+++ script/indexScript.js
@@ -2,6 +2,7 @@ let modal = document.getElementById("login_box");
 let btn= document.getElementById("modal_login");
 let span = document.getElementsByClassName("close")[0];
 let dropdownbtn= document.getElementsByClassName("modifyEvtDrop");
+const toggleBtn = document.getElementById("togglebtn");
 
 /* Modal */
 $("#modal_login").on("click", function(){
@@ -12,24 +13,67 @@ $(".close").on("click", function(){
     modal.style.display = "none";
 });
 
+//toggle button for nav bar
 $("#togglebtn").on("click", function(){
-    toggleSideBar(this);
-    if ($("#sidebar").css("display") === "none"){
-     document.getElementById("sidebar").style.display= "block";
-    }
-    else{
-     document.getElementById("sidebar").style.display= "none";
-    }
- });
+   toggleSideBar(this);
+   if ($("#sidebar").css("display") === "none"){
+    document.getElementById("sidebar").style.display= "block";
+   }
+   else{
+    document.getElementById("sidebar").style.display= "none";
+   }
+});
 
-
+//onclick
 window.onclick = function(event){
     if (event.target == modal){
         modal.style.display = "none";
     }
 }
 
+    //navBAr scroll effect
+    $(window).scroll(
+        {
+            previousTop: 0
+        },
+    function(){
+        let currentTop = $(window). scrollTop();
+        if (currentTop < this.previousTop && $(window).width()< 519){
+            $("#navbar2, #branding2").show();
+        } else{
+            $("#navbar2, #branding2").hide();
+           }
+        
+        this.previousTop=currentTop; 
+    });
 
+
+//readmore 
+    $(document).ready(function(){
+        let readMoreHtml = $(".read-more").html();
+        let lessText = readMoreHtml.substr(0,1000);
+
+        if (readMoreHtml.length>1000){
+            $(".read-more").html(lessText).append("<a href='' class='read-more-link fa'> show more...&#xf067 </a>")
+        } else {
+            $(".read-more").html(readMoreHtml);
+        }
+
+        $("body").on("click", ".read-more-link", function(event){
+            event.preventDefault();
+            $(this).parent(".read-more").html(readMoreHtml).append("<a href='' class='show-less-link fa'>show less &#xf068</a>")
+        });
+        $("body").on("click", ".show-less-link", function(event){
+           event.preventDefault();
+            $(this).parent(".read-more").html(readMoreHtml.substr(0,1000)).append("<a href='' class='read-more-link fa'>show more &#xf067 </a>")
+        });
+
+        if ($(window).width()< 519){
+           //$("#branding2").show()
+        }else {
+            //$("#branding2").hide()
+        }
+    })
 
 /* Background Animation  */
 let backgroundImages = [
@@ -66,9 +110,9 @@ function background(backgroundImages){
         document.getElementById("intro").style.backgroundImage = "url("+backgroundImages[count].img+")";
        // console.log(backgroundImages[count].img);
     },7000);
+}
 
-    function toggleSideBar(ref){
-        ref.classList.toggle('active');
-        document.getElementById('sidebar_content').classList.toggle('active');
-    }
+function toggleSideBar(ref){
+    ref.classList.toggle('active');
+    document.getElementById('sidebar_content').classList.toggle('active');
 }
