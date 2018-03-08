2545572aba7c0d8d462695b40fe81763ec64b85a
Afolabi Toluwalase <opson_ryan@yahoo.com>
2018-03-08 00:06:33 -1200

    add route for get,post,put and delete

--- server/routes/eventRoute.js
+++ server/routes/eventRoute.js
@@ -3,3 +3,14 @@
 const express = require('express')
 const eventsController = require('./../controller/eventController')
 const eventRouter = express.Router();
+
+//router for the get and  post(add)
+eventRouter.get('/',eventsController.getAllEvents)
+            .post('/',eventsController.addEvent);
+
+//router for update,get an event and delete an event
+eventRouter.put('/:eventId',eventsController.update)
+            .get('/:eventId',eventsController.getEvent)
+            .delete('/:eventId',eventsController.remove);
+
+module.exports = eventRouter;
