ab210b4e0872e062097cec74c5cce5fffd45bb15
Afolabi Toluwalase <opson_ryan@yahoo.com>
2018-03-08 00:08:51 -1200

    refactor route for get,post,put and delete

--- server/routes/centerRoute.js
+++ server/routes/centerRoute.js
@@ -3,19 +3,12 @@ const centersController = require('./../controller/centerController')
 const centerRouter = express.Router();
 
 
-//router for the get
-centerRouter.get('/',centersController.getAllCenters);
+//router for the get,post,delte and put
+centerRouter.get('/',centersController.getAllCenters)
+            .post('/',centersController.addCenter)
 
-//add a new center
-centerRouter.post('/',centersController.addCenter);
-
-//update a center
-centerRouter.put('/:centerId',centersController.updateCenter);
-
-//delete a center
-centerRouter.delete('/:centerId',centersController.removeCenter);
-
-//get a center
-centerRouter.get('/:centerId',centersController.getCenter);
+centerRouter.put('/:centerId',centersController.updateCenter)
+            .delete('/:centerId',centersController.removeCenter)
+            .get('/:centerId',centersController.getCenter);
 
 module.exports = centerRouter;
