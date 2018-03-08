b7c2c31497c112f23a87802633407797d9b632d0
Afolabi Toluwalase <opson_ryan@yahoo.com>
2018-03-08 00:00:07 -1200

    add the center model

---
+++ server/model/centerModel.js
@@ -0,0 +1,14 @@
+const centers = [
+    {
+        "id" : 1,
+        "name": "Eko-suite",
+        "cost" : "150,000"
+    },
+    {
+        "id" : 2,
+        "name": "Oriental",
+        "cost" : "150,000"
+    }
+]
+
+module.exports = centers;
