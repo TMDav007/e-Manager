35d88a4f44dd1d67e62adb185bbe84e7978e3e39
Afolabi Toluwalase <opson_ryan@yahoo.com>
2018-03-08 00:04:13 -1200

    add route for get,post,put and delete

--- server/routes/centerRoute.js
+++ server/routes/centerRoute.js
@@ -1,3 +1,21 @@
 const express = require('express')
 const centersController = require('./../controller/centerController')
 const centerRouter = express.Router();
+
+
+//router for the get
+centerRouter.get('/',centersController.getAllCenters);
+
+//add a new center
+centerRouter.post('/',centersController.addCenter);
+
+//update a center
+centerRouter.put('/:centerId',centersController.updateCenter);
+
+//delete a center
+centerRouter.delete('/:centerId',centersController.removeCenter);
+
+//get a center
+centerRouter.get('/:centerId',centersController.getCenter);
+
+module.exports = centerRouter;
