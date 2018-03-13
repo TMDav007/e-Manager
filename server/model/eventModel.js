42e6599c73369ea0443ea299529254bf71c7b87e
Afolabi Toluwalase <opson_ryan@yahoo.com>
2018-03-12 17:56:29 -1200

    converted it to es6

--- server/model/eventModel.js
+++ server/model/eventModel.js
@@ -1,30 +1,28 @@
-//this is the event model
-
-
+// this is the event model
 const events = [
-    {
-        id              : 1,
-        eventType       : "Wedding",
-        eventLocation   : "Oriental",
-        state           : "Lagos",
-        date            : "12/12/2018"
-    },
-    {
-        id              : 2,
-        eventType       : "Wedding",
-        eventLocation   : "Oriental",
-        state           : "Lagos",
-        date            : "12/12/2018"
-    },
-    {
-        id              : 3,
-        eventType       : "Wedding",
-        eventLocation   : "Oriental",
-        state           : "Lagos",
-        date            : "12/12/2018"
-    }
-]
+  {
+    id: 1,
+    eventType: 'Wedding',
+    eventLocation: 'Oriental',
+    state: 'Lagos',
+    date: '12/12/2018'
+  },
+  {
+    id: 2,
+    eventType: 'Wedding',
+    eventLocation: 'Oriental',
+    state: 'Lagos',
+    date: '12/12/2018'
+  },
+  {
+    id: 3,
+    eventType: 'Wedding',
+    eventLocation: 'Oriental',
+    state: 'Lagos',
+    date: '12/12/2018'
+  }
+];
 
 
-module.exports = events;
+export default events;
 
--- server/routes/eventRoute.js
+++ server/routes/eventRoute.js
@@ -1,14 +1,15 @@
-const express = require('express')
-const eventsController = require('./../controller/eventController')
+import express from 'express';
+import eventsController from './../controller/eventController';
+
 const eventRouter = express.Router();
 
-//router for the get and  post(add)
-eventRouter.get('/',eventsController.getAllEvents)
-            .post('/',eventsController.addEvent);
+// router for the get and  post(add)
+eventRouter.get('/', eventsController.getAllEvents)
+  .post('/', eventsController.addEvent);
 
-//router for update,get an event and delete an event
-eventRouter.put('/:eventId',eventsController.update)
-            .get('/:id',eventsController.getEvent)
-            .delete('/:id',eventsController.remove);
+// router for update,get an event and delete an event
+eventRouter.put('/:eventId', eventsController.update)
+  .get('/:id', eventsController.getEvent)
+  .delete('/:id', eventsController.remove);
 
-module.exports = eventRouter;
+export default eventRouter;
