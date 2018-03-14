5529e199b97ff6cb622947ae071dc4c43b5400fa
Afolabi Toluwalase <opson_ryan@yahoo.com>
2018-03-14 05:08:29 -1200

    modified server file

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import eventsRouter from './routes/eventRoute';
-import centersRouter from './routes/centerRoute'
+import centersRouter from './routes/centerRoute';
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT ||3000;
 
 
 app.use(bodyParser.json());
