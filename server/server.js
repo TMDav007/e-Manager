e66295c5764d12fa13ecc6e7aaf6a3a9637a4753
Afolabi Toluwalase <opson_ryan@yahoo.com>
2018-04-02 22:40:08 -1200

    modified the server file

--- server/server.js
+++ server/server.js
@@ -1,20 +1,18 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import eventsRouter from './routes/eventRoute';
-import centersRouter from './routes/centerRoute';
+import routes from './routes/index';
 
 const app = express();
 
-const port = process.env.PORT ||3000;
-
-
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/json' }));
 
-app.use('/api/events', eventsRouter);
-app.use('/api/centers', centersRouter);
+routes(app);
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+app.set('port', port);
 
 app.listen(port, () => {
   console.log('we are running live');
