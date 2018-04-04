f22323132f4455eccf6a202fd69e50830d5b16ae
Afolabi Toluwalase <opson_ryan@yahoo.com>
2018-04-03 19:47:04 -1200

    modified file

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,7 @@ module.exports = {
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: 'e-Manager',
+    database: 'database_test',
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'postgres',
